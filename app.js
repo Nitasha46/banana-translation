d958e79e2ea91c51b10669a232add5d1ba03b7ef
Nitasha <nitasha464@gmail.com>
2023-04-28 10:26:11 +0530

    chore: added function

---
+++ app.js
@@ -0,0 +1,25 @@
+var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL() {
+    return serverURL + "?" + "text=" + txtInput.value;
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something went wrong! try again after some time");
+}
+
+function clickEventHandler() {
+    fetch(getTranslationURL)
+        .then(response => response.json)
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickEventHandler)
